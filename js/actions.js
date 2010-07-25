e57576792ffd1b71c650bbad4126759059d3900b
Fernando Ferreira <fferreira@icsystems.com.br>
2010-07-25 16:36:40 -0300

    Funcoes JS  para controle do Form implementadas

--- js/actions.js
+++ js/actions.js
@@ -129,12 +129,12 @@ $(document).ready(function(){
 			}
 		}
 	});
-	//Teste do HIV
-	$('#HIVteste').change(function(){
+	//IGRA
+	$('#IGRA').change(function(){
 		var dep = new Array();
-		dep[0] = '#divDataHIV';
+		dep[0] = '#divIGRAMetodo';
 		// Se sim, disponibilizar colunas listadas a cima
-		if($(this).val()=='positivo' || $(this).val()=='negativo'){
+		if($(this).val()=='sim'){
 			for(div in dep){
 				var elems = $('*', dep[div]);
 				$(elems).each(function(){
@@ -152,7 +152,122 @@ $(document).ready(function(){
 			}
 		}
 		// Se nao, ocultar colunas listadas a cima
-		if( $(this).val() == 'ignorado'){
+		else {
+			for(div in dep){
+				var elems = $('*', dep[div]);
+				$(elems).each(function(){
+					var element = $(this);
+					if (   element[0].nodeName != 'FIELDSET'
+					    && element[0].nodeName != 'SMALL'
+					    && element[0].nodeName != 'OPTION')
+						$(this).removeClass('required');
+				});
+				if($(dep[div]).css('display') != 'none')
+					$(dep[div]).toggle();
+			}
+		}
+	});
+	//PCR
+	$('#pcr').change(function(){
+		var dep = new Array();
+		dep[0] = '#divPCRMetodo';
+		// Se sim, disponibilizar colunas listadas a cima
+		if($(this).val()=='sim'){
+			for(div in dep){
+				var elems = $('*', dep[div]);
+				$(elems).each(function(){
+					var element = $(this);
+					if (   element[0].nodeName != 'FIELDSET'
+					    && element[0].nodeName != 'SMALL'
+					    && element[0].nodeName != 'OPTION')
+						$(this).addClass('required');
+				});
+				if($(dep[div]).css('display') != 'block')
+					$(dep[div]).toggle(function() {
+						$(this).css('background-color', hlcolor);
+						$(this).animate({backgroundColor : "white"}, 4000);
+					});
+			}
+		}
+		// Se nao, ocultar colunas listadas a cima
+		else {
+			for(div in dep){
+				var elems = $('*', dep[div]);
+				$(elems).each(function(){
+					var element = $(this);
+					if (   element[0].nodeName != 'FIELDSET'
+					    && element[0].nodeName != 'SMALL'
+					    && element[0].nodeName != 'OPTION')
+						$(this).removeClass('required');
+				});
+				if($(dep[div]).css('display') != 'none')
+					$(dep[div]).toggle();
+			}
+		}
+	})
+	//Detecão TB Resistente
+	$('#detecao_tb_resistente').change(function(){
+		var dep = new Array();
+		dep[0] = '#divTbResistenteMetodo';
+		// Se sim, disponibilizar colunas listadas a cima
+		if($(this).val()=='sim'){
+			for(div in dep){
+				var elems = $('*', dep[div]);
+				$(elems).each(function(){
+					var element = $(this);
+					if (   element[0].nodeName != 'FIELDSET'
+					    && element[0].nodeName != 'SMALL'
+					    && element[0].nodeName != 'OPTION')
+						$(this).addClass('required');
+				});
+				if($(dep[div]).css('display') != 'block')
+					$(dep[div]).toggle(function() {
+						$(this).css('background-color', hlcolor);
+						$(this).animate({backgroundColor : "white"}, 4000);
+					});
+			}
+		}
+		// Se nao, ocultar colunas listadas a cima
+		else {
+			for(div in dep){
+				var elems = $('*', dep[div]);
+				$(elems).each(function(){
+					var element = $(this);
+					if (   element[0].nodeName != 'FIELDSET'
+					    && element[0].nodeName != 'SMALL'
+					    && element[0].nodeName != 'OPTION')
+						$(this).removeClass('required');
+				});
+				if($(dep[div]).css('display') != 'none')
+					$(dep[div]).toggle();
+			}
+		}
+	});
+	//HIV
+	$('#exameSida').change(function(){
+		var dep = new Array();
+		dep[0] = '#divSIDA';
+		dep[1] = '#divDataSida';
+		// Se sim, disponibilizar colunas listadas a cima
+		if($(this).val()=='sim'){
+			for(div in dep){
+				var elems = $('*', dep[div]);
+				$(elems).each(function(){
+					var element = $(this);
+					if (   element[0].nodeName != 'FIELDSET'
+					    && element[0].nodeName != 'SMALL'
+					    && element[0].nodeName != 'OPTION')
+						$(this).addClass('required');
+				});
+				if($(dep[div]).css('display') != 'block')
+					$(dep[div]).toggle(function() {
+						$(this).css('background-color', hlcolor);
+						$(this).animate({backgroundColor : "white"}, 4000);
+					});
+			}
+		}
+		// Se nao, ocultar colunas listadas a cima
+		else {
 			for(div in dep){
 				var elems = $('*', dep[div]);
 				$(elems).each(function(){
@@ -301,20 +416,75 @@ $(document).ready(function(){
 	$('#nao_testado_'+cepaNum).html(not_tested[cepaNum].toString());
 	// add row button
 	$("#addline_button").click(function(){
-		cepaNum++;
-		var content = CEPARow(cepaNum);
-		$('table.cepa').append(CEPARow(cepaNum));
-		not_tested[cepaNum] = new Array();
-		not_tested[cepaNum] = not_tested[0];
-		$('#nao_testado_'+cepaNum).html(not_tested[cepaNum].toString());
-		$('input.data_cepa').datepicker({
-			dateFormat: 'dd/mm/yy',
-			monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-			maxDate: '+0d',
-			dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
-		});
+		var origemStr = $('#origem_cepa_'+ cepaNum).val();
+		if(origemStr.replace(/-/g,'')){
+			cepaNum++;
+			var content = CEPARow(cepaNum);
+			$('table.cepa').append(CEPARow(cepaNum));
+			not_tested[cepaNum] = new Array();
+			not_tested[cepaNum] = not_tested[0];
+			$('#nao_testado_'+cepaNum).html(not_tested[cepaNum].toString());
+			$('input.data_cepa').datepicker({
+				dateFormat: 'dd/mm/yy',
+				monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+				maxDate: '+0d',
+				dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+			});
+		}
 	});
+
+	$('select.origem_cepa').live('change', function(){
+		var origemStr = $(this).val();
+		num = parseInt($(this).attr('id').split('_')[2]);
+		if(origemStr.replace(/-/g,'')){
+			$('#baciloscopia_metodo_' + num).removeAttr('disabled');
+		} else {
+			$('#baciloscopia_metodo_' + num).attr('disabled', true);
+			$('#baciloscopia_metodo_' + num).val('----');
+		}
+	});
+
+	$('select.baciloscopia_metodo').live('change',  function(){
+		var origemStr = $(this).val();
+		num = parseInt($(this).attr('id').split('_')[2]);
+		if(origemStr.replace(/-/g,'')){
+			$('#baciloscopia_data_' + num).removeAttr('disabled');
+			$('#baciloscopia_material_cepa_' + num).removeAttr('disabled');
+		} else {
+			$('#baciloscopia_data_' + num).attr('disabled', true);
+			$('#baciloscopia_data_' + num).val('');
+			$('#baciloscopia_material_cepa_' + num).attr('disabled', true);
+			$('#baciloscopia_material_cepa_' + num).val('----');
+		}
+	});
+	$('select.baciloscopia_material_cepa').live('change',  function(){
+		var origemStr = $(this).val();
+		num = parseInt($(this).attr('id').split('_')[3]);
+		console.log(origemStr);
+		if(origemStr.replace(/-/g,'')){
+			console.log('AKI-> ' +origemStr.replace(/-/g,''));
+			$('#numero_cepa_' + num).removeAttr('disabled');
+			$('#data_cepa_' + num).removeAttr('disabled');
+			$('#material_cepa_' + num).removeAttr('disabled');
+		} else {
+			console.log(origemStr.replace(/-/g,''));
+			$('#numero_cepa_' + num).attr('disabled', true);
+			$('#numero_cepa_' + num).val('----');
+			$('#data_cepa_' + num).attr('disabled', true);
+			$('#data_cepa_' + num).val('');
+			$('#material_cepa_' + num).attr('disabled', true);
+			$('#material_cepa_' + num).val('');
+		}
+	});
+
 	$('input.data_cepa').datepicker({
+		dateFormat: 'dd/mm/yy',
+		monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+		maxDate: '+0d',
+		dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+	});
+
+	$('input.baciloscopia_data').datepicker({
 		dateFormat: 'dd/mm/yy',
 		monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
 		maxDate: '+0d',
@@ -341,6 +511,7 @@ $(document).ready(function(){
 			$('#nao_testado_'+numCepa).html(not_tested[numCepa].toString());
 		}
 	});
+
 
 	$('input.input_sensibilidade').live('click', function(){
 		params = $(this).attr('id').split('_');
@@ -370,9 +541,13 @@ $(document).ready(function(){
 	// add row button
 	$("#addSoro_button").click(function(){
 		if($('#soroColetado_'+soroNum).val() == 'sim'){
-			soroNum++;
-			var content = soroRow(soroNum);
-			$('table.soro').append(content);
+			if($('#numeroSoro_'+soroNum).valid()){
+				$('#numeroSoro_'+ numSoro).attr('readonly', 'true');
+				$('#soroColetado_'+ numSoro).attr('readonly', 'true');
+				soroNum++;
+				var content = soroRow(soroNum);
+				$('table.soro').append(content);
+			}
 		}
 	});
 	$('select.soro').live('change', function(){
@@ -395,9 +570,13 @@ $(document).ready(function(){
 	// add row button
 	$("#addSangue_button").click(function(){
 		if($('#sangueColetado_'+sangueNum).val() == 'sim'){
-			sangueNum++;
-			var content = sangueRow(sangueNum);
-			$('table.sangue').append(content);
+			if($('#numeroSangue_'+sangueNum).valid()){
+				$('#numeroSangue_'+ numSangue).attr('readonly', 'true');
+				$('#sangueColetado_'+ numSangue).attr('readonly', 'true');
+				sangueNum++;
+				var content = sangueRow(sangueNum);
+				$('table.sangue').append(content);
+			}
 		}
 	});
 	$('select.sangue').live('change', function(){
@@ -427,43 +606,6 @@ $(document).ready(function(){
 		$("#layer1").hide();
 	});
 	///////////////
-
-	$('#outros_exames_1').keydown(function(){
-		if($(this).val() != '')
-			$('#outrosExames1').removeAttr('disabled');
-		else
-			$('#outrosExames1').attr('disabled', 'disabled');
-	});
-	$('#outros_exames_2').keydown(function(){
-		if($(this).val() != '')
-			$('#outrosExames2').removeAttr('disabled');
-		else
-			$('#outrosExames2').attr('disabled', 'disabled');
-	});
-	$('#outros_exames_3').keydown(function(){
-		if($(this).val() != '')
-			$('#outrosExames3').removeAttr('disabled');
-		else
-			$('#outrosExames3').attr('disabled', 'disabled');
-	});
-	$('#outros_exames_1').change(function(){
-		if($(this).val() != '')
-			$('#outrosExames1').removeAttr('disabled');
-		else
-			$('#outrosExames1').attr('disabled', 'disabled');
-	});
-	$('#outros_exames_2').change(function(){
-		if($(this).val() != '')
-			$('#outrosExames2').removeAttr('disabled');
-		else
-			$('#outrosExames2').attr('disabled', 'disabled');
-	});
-	$('#outros_exames_3').change(function(){
-		if($(this).val() != '')
-			$('#outrosExames3').removeAttr('disabled');
-		else
-			$('#outrosExames3').attr('disabled', 'disabled');
-	});
 
 	$('#form_exams').validate({
 		rules: {
